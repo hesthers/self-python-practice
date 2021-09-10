71de3d02d958f6430d6360ee5441bff29651cbd8
hesthers <h2esthers@gmail.com>
2021-09-10 11:35:21 +0900

    refactor: add more codes to improve python codes
    I added the 'for' loop statement to read a text file.

--- python_loop/file_python.py
+++ python_loop/file_python.py
@@ -1,15 +1,22 @@
 # Python codes (log data with time and loop by practicing reading and writing codes)
 
 f = open('file_log.txt', 'w', encoding='euc-kr')
-while True:
+
+i = 0
+while i <= 10000:
     import time
     from datetime import datetime as dt
-    text = f'{i} 로그 데이터: {dt.now()} \n'
+    text = f'{i}; 로그 데이터: {dt.now()} \n'
     time.sleep(2)
     f.write(text) 
 
 f = open('file_nm.txt', 'r', encoding='euc-kr')
-text_line = f.readlines()
+#text_line = f.readlines()
+txts = []
+for tx in f.readlines():
+    txts.append(tx.replace(' \n', ""))
+
 f.close()
 
 text_line
+txts
