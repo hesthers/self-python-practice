008a4a6ce4de2c9a6bc6c3948b2704527fe39734
hesthers <h2esthers@gmail.com>
2021-09-16 16:36:53 +0900

    feat: Add python codes for checking correlation
    I added the codes for regression graph of seaborn module to check the
    correlations.

--- analysis_tech/simple_regression.py
+++ analysis_tech/simple_regression.py
@@ -8,3 +8,13 @@ us_county_info = us_county[['State', 'County', 'TotalPop', 'Income', 'IncomePerC
 
 us_county_info.head()
 us_county_info.info()
+
+### Graph (Plots)
+import seaborn as sns
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(10,5))
+ax1 = fig.add_subplot(1,2,1)
+ax2 = fig.add_subplot(1,2,2)
+sns.regplot(data = us_county_info, x= 'TotalPop', y = 'Unemployment', ax=ax1)
+sns.regplot(data = us_county_info, x= 'Income', y = 'Employed', ax=ax2)
+plt.show()
