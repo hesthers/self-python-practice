1d319c086fc1c6c11295d70c3ea18969514e67cb
hesthers <h2esthers@gmail.com>
2021-09-17 16:19:41 +0900

    feat: Add the codes to check the data

--- analysis_tech/apriori_algorithm.py
+++ analysis_tech/apriori_algorithm.py
@@ -1,3 +1,8 @@
 import pandas as pd
 chipotle = pd.read_csv('./chipotle.tsv', sep='\t')
 chipotle.head()
+
+chipotle.item_name.value_counts()
+chipotle.choice_description.groupby(by=chipotle['item_name']).count()
+chipotle.quantity.groupby(by=chipotle['item_name']).count()
+chipotle.item_name.groupby(by=chipotle['order_id']).count()
