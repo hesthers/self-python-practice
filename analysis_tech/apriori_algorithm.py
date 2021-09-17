ac4081c2fbda7dc65e81ef1ab48f15cc352aebf6
hesthers <h2esthers@gmail.com>
2021-09-17 16:23:16 +0900

    feat: make the pivot table

--- analysis_tech/apriori_algorithm.py
+++ analysis_tech/apriori_algorithm.py
@@ -6,3 +6,11 @@ chipotle.item_name.value_counts()
 chipotle.choice_description.groupby(by=chipotle['item_name']).count()
 chipotle.quantity.groupby(by=chipotle['item_name']).count()
 chipotle.item_name.groupby(by=chipotle['order_id']).count()
+
+import numpy as np
+pt = pd.pivot_table(chipotle, 
+               index = 'item_name',
+               #columns = '',
+               values = 'quantity', 
+               aggfunc= np.sum,).sort_values(by ='quantity',ascending=False)
+
