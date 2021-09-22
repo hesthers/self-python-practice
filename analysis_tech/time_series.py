8093a5e65ebfc2ba8efb377bd154c0263d702d81
hesthers <h2esthers@gmail.com>
2021-09-22 11:11:28 +0900

    feat: Add the code about preprocessing of data

--- analysis_tech/time_series.py
+++ analysis_tech/time_series.py
@@ -3,3 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from fbprophet import Prophet
+
+temperature = pd.read_csv('기온데이터_서울.csv', index_col=None, header=None)
+
+temperature.drop(labels=1, axis=1, inplace=True)
+temperature.rename(columns={0:'date', 2:'avg_temp', 3:'low_temp', 4:'high_temp'}, inplace=True)
+
+tmp = temperature[['date', 'avg_temp']]
+tmp.rename(columns = {'date':'ds', 'avg_temp': 'y'}, inplace=True)
+
+tmp = tmp[tmp.ds >= '2010-01-01']
+tmp.info()
+
