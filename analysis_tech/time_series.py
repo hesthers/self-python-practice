71217ea302ff263fa22d914ebfac8e76713293a6
hesthers <h2esthers@gmail.com>
2021-09-22 11:21:40 +0900

    feat: Add the python code for the comparison
    The code is to compare the predicted data with the real data except the
    data of the 15 days later as with the visualization.

--- analysis_tech/time_series.py
+++ analysis_tech/time_series.py
@@ -42,3 +42,27 @@ r2 = r2_score(y, y_pred)
 r2
 rmse = sqrt(mean_squared_error(y, y_pred))
 rmse
+
+test_file_path = './기온데이터_서울.csv'
+temp_df = pd.read_csv(test_file_path, index_col=None, header=None)
+
+temp_df.drop(labels=1, axis=1, inplace=True)
+temp_df.rename(columns={0:'date', 2:'avg_temp', 3:'low_temp', 4:'high_temp'}, inplace=True)
+
+tmp_df = temp_df[['date', 'avg_temp']]
+tmp_df.rename(columns = {'date':'ds', 'avg_temp': 'y'}, inplace=True)
+
+tmp_df = tmp_df[tmp_df.ds >= '2010-01-01']
+
+y = tmp_df.y.values[-15:]
+y_pred = forecast_data.yhat.values[-30:-15]
+date = tmp_df.ds.values[-15:]
+
+sns.lineplot(data = forecast_data[-30:-15], x= date, y= y_pred, label = 'predicted')
+sns.lineplot(data = tmp_df[-15:], x= date, y = y, label = 'real')
+plt.xticks(rotation = 75)
+plt.ylim((10, 40))
+plt.legend()
+plt.show()
+
+
